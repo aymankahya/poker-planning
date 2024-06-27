043108e8cb3d9675cd6eae2d64bd2a941d5763ea
Ayman Kahya <kahya.ayman@gmail.com>
2024-06-27 22:23:48 +0200

    deploy(client): prepare client files for deployment

--- server/validation/index.ts
+++
@@ -1,8 +0,0 @@
-export { default as validateUserLoginInfo } from '@/validation/authentication/userLoginValidation';
-export { default as validateUserRegisterInfo } from '@/validation/authentication/userSignupValidation';
-export { default as guestNameValidation } from '@/validation/authentication/guestNameValidation';
-export { default as createSessionValidation } from '@/validation/session/createSessionValidation';
-export { default as createNewIssueValidation } from '@/validation/session/createNewIssueValidation';
-export { default as joinSessionValidation } from '@/validation/session/joinSessionValidation';
-export { default as createCustomVotingValidation } from '@/validation/session/createCustomVotingValidation';
-export { default as updateSessionSettingsValidation } from '@/validation/session/updateSessionSettingsValidation';
